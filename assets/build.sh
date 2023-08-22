8471fa8993c5c3296dfba0d56789738193b8b4b6
Jake Ireland <jakewilliami@icloud.com>
2023-08-23 01:05:25 +1200

    Flatten formulae assets

--- assets/build.sh
+++ assets/build.sh
@@ -5,7 +5,7 @@ set -xe
 LFLAGS="--file-line-error --synctex=1 --enable-write18"
 PFLAGS="-verbose -density 500 -trim -quality 100 -sharpen 0x1.0"
 pdflatex $LFLAGS formulae.tex
-convert $PFLAGS formulae.pdf formulae.png
+convert $PFLAGS -flatten formulae.pdf formulae.png
 
 PDIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 julia --project="$PDIR" plot.jl
